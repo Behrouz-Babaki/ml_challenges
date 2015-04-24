19820e4b0032dd38e50d210610ac3b1d625da6ec
Behrouz Babaki <Behrouz.Babaki@cs.kuleuven.be>
2015-04-24 03:16:07 +0200

    a submission which brought slight improvement

---
+++ bikes/models/model_5_weekday.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+
+def get_features(data):
+    return data[['season',
+                 'holiday',
+                 'workingday',
+                 'weather',
+                 'temp',
+                 'atemp',
+                 'humidity',
+                 'windspeed',
+                 'hour', 
+                 'weekday'
+                 ]]
+
+def main():
+	train = pd.read_csv('../train.csv', parse_dates=['datetime'])
+	train['hour'] = pd.DatetimeIndex(train['datetime']).hour
+	train['weekday'] = pd.DatetimeIndex(train['datetime']).weekday
+        
+	test = pd.read_csv('../test.csv', parse_dates=['datetime'])
+	test['hour'] = pd.DatetimeIndex(test['datetime']).hour
+	test['weekday'] = pd.DatetimeIndex(test['datetime']).weekday
+
+
+	results = pd.DataFrame(columns=['datetime', 'count'])	
+	for hour, test_subset in test.groupby(test['hour']):
+	    train_subset = train[train['hour'] == hour]
+	    model = RandomForestRegressor(n_estimators=100)
+	    model.fit(np.array(get_features(train_subset)), np.array(train_subset['count']))
+	    predictions = model.predict(np.array(get_features(test_subset)))
+	    dt = test_subset['datetime']
+	    predictions = pd.Series(predictions, index=dt.index)
+	    res = pd.concat([dt, predictions], axis=1)
+	    res.columns=['datetime', 'count']
+	    results = pd.concat([results, res])
+
+	results['count'] = results['count'].astype('int64')
+	results = results.sort('datetime')
+	results.to_csv('../submissions/seventhSubmission.csv', index=False)
+
+
+if __name__ == '__main__':
+	main()
