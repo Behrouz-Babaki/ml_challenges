f076228d2bb34d6c43cd9ac2a19a610b90974879
Behrouz Babaki <Behrouz.Babaki@cs.kuleuven.be>
2015-04-24 03:55:53 +0200

    adding isweekend as a feature. not submitted

--- bikes/models/model_5_weekday.py
+++ bikes/models/model_5_weekday.py
@@ -14,17 +14,22 @@ def get_features(data):
                  'humidity',
                  'windspeed',
                  'hour', 
-                 'weekday'
+                 'weekday',
+                 'isweekend'
                  ]]
 
 def main():
 	train = pd.read_csv('../train.csv', parse_dates=['datetime'])
 	train['hour'] = pd.DatetimeIndex(train['datetime']).hour
 	train['weekday'] = pd.DatetimeIndex(train['datetime']).weekday
+        train['isweekend'] = 0
+        train.loc[(train['weekday']==5) | (train['weekday']==6), 'isweekend'] = 1
         
 	test = pd.read_csv('../test.csv', parse_dates=['datetime'])
 	test['hour'] = pd.DatetimeIndex(test['datetime']).hour
 	test['weekday'] = pd.DatetimeIndex(test['datetime']).weekday
+        test['isweekend'] = 0
+        test.loc[(test['weekday']==5) | (test['weekday']==6), 'isweekend'] = 1
 
 
 	results = pd.DataFrame(columns=['datetime', 'count'])	
